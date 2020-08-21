6877d04e7fb4708612c7849f4bb5483e5909ef13
TDaryaT <tdaryat@gmail.com>
2020-08-21 19:59:31 +0300

    Echo bot added, readme and .gitignore init

---
+++ README.md
@@ -0,0 +1,10 @@
+#My Weather bot for Telegram
+
+## Was Used:
+* Telegram Bot Java Library - [TelegramBots 4.9.1](https://github.com/rubenlagus/TelegramBots)
+
+* [SE JDK 14](https://www.oracle.com/java/technologies/javase-downloads.html)
+
+* IDE - [IntelliJ IDEA](https://www.jetbrains.com/idea/download/#section=linux)
+
+ 
--- src/main/java/MyWeatherTgBot.java
+++ src/main/java/MyWeatherTgBot.java
@@ -1,6 +1,7 @@
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class MyWeatherTgBot extends TelegramLongPollingBot {
     private static final String BOT_TOKEN = "";
@@ -8,12 +9,19 @@ public class MyWeatherTgBot extends TelegramLongPollingBot {
 
     public MyWeatherTgBot(){}
 
-    public MyWeatherTgBot(DefaultBotOptions options){
-        super(options);
-    }
-
+    @Override
     public void onUpdateReceived(Update update) {
-
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText("You said: " + update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
