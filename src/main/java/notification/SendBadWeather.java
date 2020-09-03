385cc7e225b093bf9788296bfe98017c7ad46280
TDaryaT <tdaryat@gmail.com>
2020-09-03 19:11:38 +0300

    delete folder

---
+++ src/main/java/notification/SendBadWeather.java
@@ -0,0 +1,20 @@
+package Commands;
+
+import bot.MyWeatherTgBot;
+import org.quartz.*;
+
+public class NotUpdateCommand implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        /* Retrieve the bot instance */
+        SchedulerContext schedulerContext = null;
+        try {
+            schedulerContext = jobExecutionContext.getScheduler().getContext();
+        } catch (SchedulerException e1) {
+            e1.printStackTrace();
+        }
+        MyWeatherTgBot bot = (MyWeatherTgBot) schedulerContext.get("bot");
+
+        bot.sendNotification();
+    }
+}
